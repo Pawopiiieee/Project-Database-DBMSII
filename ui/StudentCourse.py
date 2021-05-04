e6a16e0928a353187a141c37e0b5be65c0d461d6
Pawopiiieee <paszck@gmail.com>
2021-05-04 22:37:35 +0200

    student course fixed

--- ui/StudentCourse.py
+++ ui/StudentCourse.py
@@ -13,11 +13,11 @@ def student_personal_course(window, return_function):
 	header_label = Label(window,text = "Course Name", fg = "#00293c", font = "Impact 35", bg ="#d9b44a", height = 2, width = 15)
 	header_label.place(x = 105, y = 40)
 
-	code_label = Label(window,text = "Code", fg = "#00293c", font = "Courier 15", highlightbackground ="#acd0c0", height = 2,width = 15)
+	code_label = Label(window,text = "Code", fg = "#00293c", font = "Courier 15", highlightbackground ="#acd0c0", height = 2,width = 14,borderwidth=2, relief="groove")
 	code_label.place(x = 35, y = 200)
-	subject_label = Label(window,text = "Subject", fg = "#00293c", font = "Courier 15", highlightbackground ="#acd0c0", height = 2,width = 25)
+	subject_label = Label(window,text = "Subject", fg = "#00293c", font = "Courier 15", highlightbackground ="#acd0c0", height = 2,width = 22,borderwidth=2, relief="groove")
 	subject_label.place(x = 150, y = 200)
-	exam_label = Label(window,text = "Exam", fg = "#00293c", font = "Courier 15", highlightbackground ="#acd0c0", height = 2,width = 12)
+	exam_label = Label(window,text = "Exam", fg = "#00293c", font = "Courier 15", highlightbackground ="#acd0c0", height = 2,width = 12,borderwidth=2, relief="groove")
 	exam_label.place(x = 350, y = 200)
 
 
